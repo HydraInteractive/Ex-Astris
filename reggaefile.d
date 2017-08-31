e39a200b2171cea9eaea6e796548272020340214
Dan Printzell <xwildn00bx@gmail.com>
2017-08-31 13:15:41 +0200

    Moved to separate dynamic libs, example -> barcode
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- reggaefile.d
+++ reggaefile.d
@@ -1,16 +1,28 @@
 import reggae;
+import std.algorithm;
+import std.array;
+import std.conv;
 
-enum CFlagsLib = "-O0 -std=c++14 -ffat-lto-objects -ggdb -Wall -Werror -fdiagnostics-color=always -fopenmp -fPIC -Ihydra/include";
-enum CFlagsExec = "-O0 -std=c++14 -ffat-lto-objects -ggdb -Wall -Werror -fdiagnostics-color=always -fopenmp -fPIC -Ihydra/include -Iexample/include";
-enum LFlagsLib = "-O0 -shared  -Wl,--no-undefined -ggdb -fdiagnostics-color=always -fopenmp -lm -ldl -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf -lGL -lassimp";
-enum LFlagsExec = "-O0 -Wl,--no-undefined -Wl,-rpath,. -Wl,-rpath,objs/examplegame.objs -Lobjs/examplegame.objs -lhydra -ggdb -fdiagnostics-color=always -fopenmp";
+enum string[] SubProjects = ["hydra", "hydra_graphics", "hydra_network", "hydra_physics", "hydra_sound"];
+enum string SubProjectsInclude = SubProjects.map!((string x) => "-I" ~ x ~ "/include").joiner(" ").array.to!string;
+enum string SubProjectsLink = SubProjects.map!((string x) => "-l" ~ x).joiner(" ").array.to!string;
 
-enum CompileCommand {
+enum string CFlagsLib = "-O0 -std=c++14 -ffat-lto-objects -ggdb -Wall -Werror -fdiagnostics-color=always -fopenmp -fPIC " ~ SubProjectsInclude;
+enum string CFlagsExec = "-O0 -std=c++14 -ffat-lto-objects -ggdb -Wall -Werror -fdiagnostics-color=always -fopenmp -fPIC -Ibarcode/include " ~ SubProjectsInclude;
+
+enum LFlagsHydraLib = "-O0 -shared -Wl,--no-undefined -Wl,-rpath,objs/barcodegame.objs -Lobjs/barcodegame.objs -ggdb -fdiagnostics-color=always -fopenmp -lm -ldl -lSDL2";
+enum LFlagsGraphicsLib = "-O0 -shared -Wl,--no-undefined -Wl,-rpath,objs/barcodegame.objs -Lobjs/barcodegame.objs -ggdb -fdiagnostics-color=always -fopenmp -lhydra -lGL -lSDL2 -lSDL2_image -lSDL2_ttf -lassimp";
+enum LFlagsNetworkLib = "-O0 -shared -Wl,--no-undefined -Wl,-rpath,objs/barcodegame.objs -Lobjs/barcodegame.objs -ggdb -fdiagnostics-color=always -fopenmp -lhydra";
+enum LFlagsPhysicsLib = "-O0 -shared -Wl,--no-undefined -Wl,-rpath,objs/barcodegame.objs -Lobjs/barcodegame.objs -ggdb -fdiagnostics-color=always -fopenmp -lhydra";
+enum LFlagsSoundLib = "-O0 -shared -Wl,--no-undefined -Wl,-rpath,objs/barcodegame.objs -Lobjs/barcodegame.objs -ggdb -fdiagnostics-color=always -fopenmp -lSDL2_mixer";
+enum LFlagsExec = "-O0 -Wl,--no-undefined -Wl,-rpath,objs/barcodegame.objs -Lobjs/barcodegame.objs -ggdb -fdiagnostics-color=always -fopenmp " ~ SubProjectsLink;
+
+enum CompileCommand : string {
 	CompileLib = "g++ -c " ~ CFlagsLib ~ " $in -o $out",
 	CompileExec = "g++ -c " ~ CFlagsExec ~ " $in -o $out",
-	LinkLib = "g++ " ~ LFlagsLib ~ " $in -o $out",
 	LinkExec = "g++ " ~ LFlagsExec ~ " $in -o $out"
 }
+enum string Link(string lib) = "g++ " ~ lib ~ " $in -o $out";
 
 Target[] MakeObjects(string src, CompileCommand cmd)() {
 	import std.file : dirEntries, SpanMode;
@@ -40,8 +52,13 @@ Target[] MakeObjects(string src, CompileCommand cmd)() {
 }
 
 Build myBuild() {
-	auto libhydra = Target("libhydra.so", CompileCommand.LinkLib, MakeObjects!("hydra/src/", CompileCommand.CompileLib));
-	auto example = Target("examplegame", CompileCommand.LinkExec, MakeObjects!("example/src/", CompileCommand.CompileExec), [libhydra]);
+	auto libhydra = Target("libhydra.so", Link!(LFlagsHydraLib), MakeObjects!("hydra/src/", CompileCommand.CompileLib));
+	auto libhydra_graphics = Target("libhydra_graphics.so", Link!(LFlagsGraphicsLib), MakeObjects!("hydra_graphics/src/", CompileCommand.CompileLib), [libhydra]);
+	auto libhydra_network = Target("libhydra_network.so", Link!(LFlagsNetworkLib), MakeObjects!("hydra_network/src/", CompileCommand.CompileLib), [libhydra]);
+	auto libhydra_physics = Target("libhydra_physics.so", Link!(LFlagsPhysicsLib), MakeObjects!("hydra_physics/src/", CompileCommand.CompileLib), [libhydra]);
+	auto libhydra_sound = Target("libhydra_sound.so", Link!(LFlagsSoundLib), MakeObjects!("hydra_sound/src/", CompileCommand.CompileLib), [libhydra]);
 
-	return Build(example);
+	auto barcode = Target("barcodegame", CompileCommand.LinkExec, MakeObjects!("barcode/src/", CompileCommand.CompileExec), [libhydra, libhydra_graphics, libhydra_network, libhydra_physics, libhydra_sound]);
+
+	return Build(barcode);
 }
