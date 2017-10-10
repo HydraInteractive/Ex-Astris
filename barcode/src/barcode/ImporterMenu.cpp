cef2b5ea999382b5197d3e4c2cbe89ab44445a81
Karl Essinger <xkaess22@gmail.com>
2017-10-10 12:13:17 +0200

    Emil is killing me, switching to pathfinding

--- barcode/src/barcode/ImporterMenu.cpp
+++ barcode/src/barcode/ImporterMenu.cpp
@@ -26,7 +26,7 @@ ImporterMenu::~ImporterMenu()
 void ImporterMenu::render(bool &closeBool, Hydra::Renderer::Batch& previewBatch, float delta)
 {
 	ImGui::SetNextWindowSize(ImVec2(1000, 700), ImGuiSetCond_Once);
-	ImGui::Begin("Import", &closeBool, ImGuiWindowFlags_MenuBar);
+	ImGui::Begin("Import", &closeBool);
 	if (ImGui::BeginMenuBar())
 	{
 		if (ImGui::BeginMenu("Menu"))
