889d4b96d9e0e2215ad3455b752c6f99796df66b
Karl Essinger <xkaess22@gmail.com>
2017-11-03 15:59:24 +0100

    Revert "Code recovered from other branch"

--- hydra_physics/src/component/aicomponent.cpp
+++ hydra_physics/src/component/aicomponent.cpp
@@ -20,25 +20,32 @@ AIComponent::~AIComponent() {
 
 void AIComponent::serialize(nlohmann::json& json) const {
 	json = {
+		{ "scale",{ _scale.x, _scale.y, _scale.z } },
 		{ "behaviourType", (unsigned int)behaviour->type},
-		{ "radius",{ radius } },
-		{ "pathState", behaviour->state },
+		//{ "pathState", behaviour->state },
 		{ "damage", damage },
-		{ "range", behaviour->range },
-		{ "original range", behaviour->originalRange }
+		{ "range", range },
+		{ "mapOffset",{ mapOffset.x, mapOffset.y, mapOffset.z } },
+		{ "Original range", originalRange }
 	};
 
-	for (size_t i = 0; i < MAP_SIZE; i++)
+	for (size_t i = 0; i < 64; i++)
 	{
-		for (size_t j = 0; j < MAP_SIZE; j++)
+		for (size_t j = 0; j < 64; j++)
 		{
-			json["map"][i][j] = behaviour->map[i][j];
+			json["map"][i][j] = map[i][j];
 		}
 	}
 }
 
 void AIComponent::deserialize(nlohmann::json& json) {
-		Behaviour::Type behaviourType = (Behaviour::Type)json["behaviourType"].get<unsigned int>();
+	auto& scale = json["scale"];
+	_scale = glm::vec3{ scale[0].get<float>(), scale[1].get<float>(), scale[2].get<float>() };
+
+	auto& _mapOffset = json["mapOffset"];
+	mapOffset = glm::vec3{ _mapOffset[0].get<float>(), _mapOffset[1].get<float>(), _mapOffset[2].get<float>() };
+
+	Behaviour::Type behaviourType = (Behaviour::Type)json["behaviourType"].get<unsigned int>();
 	switch (behaviourType)
 	{
 	case Behaviour::Type::ALIEN:
@@ -54,20 +61,18 @@ void AIComponent::deserialize(nlohmann::json& json) {
 		std::cout << "Invalid AI Behaviour Type" << std::endl;
 		break;
 	}
-	radius = json["radius"].get<float>();
-
-	behaviour->state = json["pathState"].get<int>();
+	//behaviour->state = json["pathState"].get<int>();
 	damage = json["damage"].get<int>();
 
 
-	behaviour->range = json["range"].get<float>();
-	behaviour->originalRange = json["original range"].get<float>();
+	range = json["range"].get<float>();
+	originalRange = json["Original range"].get<float>();
 
-	for (size_t i = 0; i < MAP_SIZE; i++)
+	for (size_t i = 0; i < 64; i++)
 	{
-		for (size_t j = 0; j < MAP_SIZE; j++)
+		for (size_t j = 0; j < 64; j++)
 		{
-			behaviour->map[i][j] = json["map"][i][j].get<int>();
+			map[i][j] = json["map"][i][j].get<int>();
 		}
 	}
 }
@@ -76,9 +81,85 @@ void AIComponent::deserialize(nlohmann::json& json) {
 // Note: This function won't always be called
 void AIComponent::registerUI() {
 	ImGui::InputInt("pathState", &debugState);
-	ImGui::Checkbox("isAtGoal", &behaviour->isAtGoal);
-	ImGui::Checkbox("playerCanBeSeen", &behaviour->playerSeen);
-	ImGui::InputFloat("range", &behaviour->range);
+	ImGui::Checkbox("isAtGoal", &isAtGoal);
+	ImGui::Checkbox("playerCanBeSeen", &playerSeen);
+	ImGui::InputFloat("range", &range);
+}
+
+bool Hydra::Component::AIComponent::checkLOS(int levelmap[WORLD_SIZE][WORLD_SIZE], glm::vec3 A, glm::vec3 B)
+{
+	// New code, not optimal
+	double x = B.x - A.x;
+	double z = B.z - A.z;
+	double len = std::sqrt((x*x) + (z*z));
+
+	if (!len) // Player tile is same as target tile
+		return true;
+
+	double unitx = x / len;
+	double unitz = z / len;
+
+	x = A.x;
+	z = A.z;
+	for (double i = 1; i < len; i += 1)
+	{
+		if (levelmap[(int)x][(int)z] == 1)
+		{
+			return false;
+		}
+
+		x += unitx;
+		z += unitz;
+	}
+
+	return true;
+
+	// Old code
+	/*bool steep = (fabs(B.z - A.z) > fabs(B.x - A.x));
+	if (steep)
+	{
+		std::swap(A.x, A.z);
+		std::swap(B.x, B.z);
+	}
+
+	if (A.x > B.x)
+	{
+		std::swap(A.x, B.x);
+		std::swap(A.z, B.z);
+	}
+
+	float dx = B.x - A.x;
+	float dz = fabs(B.z - A.z);
+
+	float error = dx / 2.0f;
+	int zStep = (A.z < B.z) ? 1 : -1;
+	int z = (int)A.z;
+
+	int maxX = (int)B.x;
+
+	int x;
+	for (x = (int)A.x; x < maxX; x++)
+	{
+		if (steep)
+		{
+			map[x][z] = 3;
+			if (levelmap[x][z] == 1) return false;
+		}
+		else
+		{
+			map[x][z] = 3;
+			if (levelmap[z][x] == 1) return false;
+		}
+
+		error -= dz;
+		if (error < 0)
+		{
+			z += zStep;
+			error += dx;
+		}
+	}
+
+	return true;*/
 }
 
 std::shared_ptr<Hydra::World::Entity> AIComponent::getPlayerEntity()
