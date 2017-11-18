35e0e05531120a9966e388f12ac6234488927653
Zather1995 <zather1995@gmail.com>
2017-11-18 11:40:59 +0100

    real rotation for editor state

--- barcode/src/barcode/editorstate.cpp
+++ barcode/src/barcode/editorstate.cpp
@@ -140,7 +140,7 @@ namespace Barcode {
 				glm::vec3 skew;
 				glm::vec4 perspective;
 				glm::decompose(drawObj->modelMatrix, newScale, rotation, translation, skew, perspective);
-				_hitboxBatch.batch.objects[_hitboxCube.get()].push_back(glm::translate(translation) * glm::mat4_cast(rotation) * glm::scale(goc->halfExtents * glm::vec3(2)));
+				_hitboxBatch.batch.objects[_hitboxCube.get()].push_back(glm::translate(translation) * glm::mat4_cast(goc->quatRotation) * glm::scale(goc->halfExtents * glm::vec3(2)));
 			}
 
 			_hitboxBatch.pipeline->setValue(0, _cc->getViewMatrix());
