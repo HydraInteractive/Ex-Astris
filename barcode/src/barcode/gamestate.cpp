a73c86f86ae8026b061279f9d942114ac29efa62
ChobitsTheZero <Emil.Nordlander@hotmail.com>
2017-11-20 11:08:46 +0100

    starterroom is now actually starter room

--- barcode/src/barcode/gamestate.cpp
+++ barcode/src/barcode/gamestate.cpp
@@ -395,7 +395,7 @@ namespace Barcode {
 		}
 		{
 			//Remove this to gain frames like never before
-			tileGen = new TileGeneration("assets/room/threewayRoom.room");
+			tileGen = new TileGeneration("assets/room/starterRoom.room");
 			pathfindingMap = tileGen->buildMap();
 		}
 	/*	{
