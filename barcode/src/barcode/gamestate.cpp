3a8b3044a82af6708f042ff086ebbeaef1dd9f47
Walter Karlqvist <walter.karlqvist@gmail.com>
2017-11-27 15:57:50 +0100

    almost working icons

--- barcode/src/barcode/gamestate.cpp
+++ barcode/src/barcode/gamestate.cpp
@@ -360,22 +360,29 @@ namespace Barcode {
 			{
 				char buf[128];
 				snprintf(buf, sizeof(buf), "Perk%lu", i);
+				Hydra::Component::PerkComponent::PERK_DMGUPSIZEUP;
 				float xOffset = float((-10 * amountOfPerks) + (20 * i));
 				ImGui::SetNextWindowPos(pos + ImVec2(xOffset, +480));
 				ImGui::SetNextWindowSize(ImVec2(20, 20));
 				ImGui::Begin(buf, NULL, ImGuiWindowFlags_::ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_::ImGuiWindowFlags_NoResize | ImGuiWindowFlags_::ImGuiWindowFlags_NoMove);
 				switch (perk->activePerks[i])
 				{
-				case :
+				case Hydra::Component::PerkComponent::PERK_DMGUPSIZEUP:
 					ImGui::Image(reinterpret_cast<ImTextureID>(_textureLoader->getTexture("assets/hud/BulletVelocity.png")->getID()), ImVec2(20, 20));
 					break;
-				case BUFF_DAMAGEUPGRADE:
+				case Hydra::Component::PerkComponent::PERK_GRENADE:
 					ImGui::Image(reinterpret_cast<ImTextureID>(_textureLoader->getTexture("assets/hud/DamageUpgrade.png")->getID()), ImVec2(20, 20));
 					break;
-				case BUFF_HEALING:
+				case Hydra::Component::PerkComponent::PERK_MINE:
 					ImGui::Image(reinterpret_cast<ImTextureID>(_textureLoader->getTexture("assets/hud/Healing.png")->getID()), ImVec2(20, 20));
 					break;
-				case BUFF_HEALTHUPGRADE:
+				case Hydra::Component::PerkComponent::PERK_BULLETSPRAY:
+					ImGui::Image(reinterpret_cast<ImTextureID>(_textureLoader->getTexture("assets/hud/HealthUpgrade.png")->getID()), ImVec2(20, 20));
+					break;
+				case Hydra::Component::PerkComponent::PERK_SPEEDUP:
+					ImGui::Image(reinterpret_cast<ImTextureID>(_textureLoader->getTexture("assets/hud/HealthUpgrade.png")->getID()), ImVec2(20, 20));
+					break;
+				case Hydra::Component::PerkComponent::PERK_FASTSHOWLOWDMG:
 					ImGui::Image(reinterpret_cast<ImTextureID>(_textureLoader->getTexture("assets/hud/HealthUpgrade.png")->getID()), ImVec2(20, 20));
 					break;
 				}
