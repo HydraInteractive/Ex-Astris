8f2b580d16d7016b34db39d4088966351e77bb88
ChobitsTheZero <Emil.Nordlander@hotmail.com>
2017-12-06 16:28:48 +0100

    nice rooms, beautiful pathing

--- barcode/src/barcode/gamestate.cpp
+++ barcode/src/barcode/gamestate.cpp
@@ -468,7 +468,7 @@ namespace Barcode {
 		{
 			//Remove this to gain frames like never before
 
-			tileGen = new TileGeneration("assets/room/threedoors.room");
+			tileGen = new TileGeneration("assets/room/tryTwo.room");
 			pathfindingMap = tileGen->buildMap();
 		}
 
