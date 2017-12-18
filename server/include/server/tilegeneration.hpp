7e4ad8bde3f56ce28767386a27a28d417465e694
ChobitsTheZero <Emil.Nordlander@hotmail.com>
2017-12-18 11:37:08 +0100

    fixing spawners

--- server/include/server/tilegeneration.hpp
+++ server/include/server/tilegeneration.hpp
@@ -48,6 +48,7 @@ namespace BarcodeServer {
 		void spawnDoors();
 		void spawnPickUps();
 		void spawnEnemies();
+		void createSpawner();
 		void finalize();
 
 		std::string getPathMapAsString();
@@ -70,7 +71,6 @@ namespace BarcodeServer {
 		void _randomizeRooms();
 		bool _generatePlayerSpawnPoints();
 		void _spawnRandomEnemy(glm::vec3 pos);
-		void _createSpawner(std::shared_ptr<Hydra::World::Entity>& room, int id);
 		void _clearSpawnPoints();
 		void _spawnLight(std::shared_ptr<Hydra::Component::TransformComponent>& roomTransform);
 		glm::quat _rotateRoom(std::shared_ptr<Hydra::Component::RoomComponent>& room, uint8_t& rot);
